9722c48ca3ad3dea6a275444068ccbba285b0092
lc4t <lc4t0.0@gmail.com>
2016-02-23 13:11:06 +0800

    DFS

--- decoder.py
+++ decoder.py
@@ -2,6 +2,7 @@
 #coding: utf-8
 Author = 'lc4t'
 DEBUG = True
+ENCODING = 'utf-8'
 
 import sys
 from functions import * 
@@ -10,20 +11,39 @@ from functions import *
 class Decode:
     def __init__(self, guessString):
         self.guessString = guessString
-        self.method = ['Base16', 'Base32', 'Base64']
+        self.method = ['Base16Decode', 'Base32Decode', 'Base64Decode']
         self.method.sort()
-        self.echo()
+        self.total = 0
+        # self.echo()
+
+    def count(self):
+        print ('all answer:', self.total)
 
     def echo(self):
-        total = 0
+        self.total = 0
         for seq in range(0, len(self.method), 1):
                 answer = eval(self.method[seq])(self.guessString, DEBUG)
                 if (len(answer) == 0):
                     continue
                 else:
-                    print (self.method[seq] + ':' + answer)
-                total += 1
-        print ('all answer:', total)
+                    print (self.method[seq] + '->' + answer)
+                self.total += 1
+        self.count()
+
+    def DFS(self, string, encryption = ''):
+        if (len(string) == 0):
+            return
+        for one in range(0, len(self.method), 1):
+            answer = eval(self.method[one])(string, DEBUG, ENCODING)
+            
+            if (len(answer) > 0):
+                self.total += 1
+                encryption = encryption + self.method[one] + ' -> '
+                print (encryption + answer)
+                self.DFS(answer, encryption)
+            else:
+                continue
+        
 
 
 def main():
@@ -35,8 +55,8 @@ def main():
         print (usage)
         exit(0)
     guessString = sys.argv[1]
-    
-    Decode(guessString)
+
+    Decode(guessString).DFS(guessString)
 
 
 
--- functions.py
+++ functions.py
@@ -2,14 +2,42 @@
 #coding: utf-8
 Author = 'lc4t'
 
+
+# def okChar(t):
+
+
+#     if t in [9, 10, 13]:
+#         return True
+
+#     if (32 <= i <= 126):
+#         return True
+
+#     return False
+
 import base64
-def Base16(guessString, DEBUG = False):
+
+def checkAcceptableCharacter(ans, function, ENCODING = 'utf-8'):
+    AcceptableList = [9, 10, 13]
+    if (ENCODING == 'utf-8'):
+            for i in ans:
+                if ((ord(i) not in AcceptableList) or not (32 <= i <= 126)):
+                    raise ValueError('Error in ' + function + ':' + i)
+                    return False
+
+
+def Base16Decode(guessString, DEBUG = False, ENCODING = 'utf-8'):
     ans = ''
     try:
-        ans = base64.b16decode(guessString).decode()
+        ans = base64.b16decode(guessString).decode(ENCODING)
+        checkAcceptableCharacter(ans, 'Base16Decode', ENCODING = 'utf-8')
+        # if (ENCODING == 'utf-8'):
+        #     for i in ans:
+        #         if (not okChar(ord(i))):
+        #             raise ValueError('Error in Base16Decode:'+i)
+        #             return ''
     except Exception as e:
         if (DEBUG):
-            print ('-----Error in Base16-----')
+            print ('-----Error in Base16Decode-----')
             print (e)
             print ('-------------------------')
         else:
@@ -18,26 +46,38 @@ def Base16(guessString, DEBUG = False):
 
 
 
-def Base32(guessString, DEBUG = False):
+def Base32Decode(guessString, DEBUG = False, ENCODING = 'utf-8'):
     ans = ''
     try:
-        ans = base64.b32decode(guessString).decode()
+        ans = base64.b32decode(guessString).decode(ENCODING)
+        # if (ENCODING == 'utf-8'):
+        #     for i in ans:
+        #         if (not okChar(ord(i))):
+        #             raise ValueError('Error in Base32Decode:'+i)
+        #             return ''
+        checkAcceptableCharacter(ans, 'Base32Decode', ENCODING = 'utf-8')
     except Exception as e:
         if (DEBUG):
-            print ('-----Error in Base32-----')
+            print ('-----Error in Base32Decode-----')
             print (e)
             print ('-------------------------')
         else:
             pass
     return ans
 
-def Base64(guessString, DEBUG = False):
+def Base64Decode(guessString, DEBUG = False, ENCODING = 'utf-8'):
     ans = ''
     try:
-        ans = base64.b64decode(guessString).decode()
+        ans = base64.b64decode(guessString).decode(ENCODING)
+        # if (ENCODING == 'utf-8'):
+        #     for i in ans:
+        #         if (not okChar(ord(i))):
+        #             raise ValueError('Error in Base64Decode:'+i)
+        #             return ''
+        checkAcceptableCharacter(ans, 'Base64Decode', ENCODING = 'utf-8')
     except Exception as e:
         if (DEBUG):
-            print ('-----Error in Base64-----')
+            print ('-----Error in Base64Decode-----')
             print (e)
             print ('-------------------------')
         else:
